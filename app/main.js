dc9d173c6c0b36bf4cdb10ce5f0b7b28ccc56cca
Arun Bohra <arunbohra12@gmail.com>
2025-02-07 21:40:44 +0530

    response body

--- app/main.js
+++ app/main.js
@@ -1,10 +1,42 @@
 const net = require("net");
+const HTTP = require("./http");
+
+const formatHeaders = (headers) => {
+  let formattedHeader = "";
+  for (const [headerKey, headerValue] of Object.entries(headers)) {
+    formattedHeader += `${headerKey}: ${headerValue}\r\n`;
+  }
+  return formattedHeader;
+};
+
+const registeredPaths = ["/", "/echo/:text"];
+const matchRegisteredPath = (path) => {
+  return registeredPaths.find((currentPath) => currentPath === path);
+};
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const path = data.toString().split(" ")[1];
-    const responseStatus = path === "/" ? "200 OK" : "404 NOT Found";
-    socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
+    let responseBody = "";
+
+    const headers = {
+      "Content-Type": "text/plain",
+      "Content-Length": 3,
+    };
+
+    const isPathRegistered = matchRegisteredPath(path);
+    const statusCode = isPathRegistered ? HTTP.HTTP_OK : HTTP.HTTP_NOT_FOUND;
+    const responseStatus = HTTP.getResponseStatus(statusCode);
+
+    if (path.indexOf("/echo/") === 0) {
+      const text = path.split("/")[2];
+      responseBody += text;
+    }
+
+    const formattedHeaders = formatHeaders(headers);
+    socket.write(
+      `HTTP/1.1 ${responseStatus}\r\n\r\n${formattedHeaders}\r\n\r\n${responseBody}`
+    );
   });
 
   socket.on("close", () => {
