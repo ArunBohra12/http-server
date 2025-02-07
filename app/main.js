db753d6bad4d7e90f2bd27a9c46068be67ab2d66
Arun Bohra <arunbohra12@gmail.com>
2025-02-07 20:58:06 +0530

    remove comment

--- app/main.js
+++ app/main.js
@@ -2,7 +2,6 @@ const net = require("net");
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    // console.log(data.toString().split("\r\n")[0]);
     const path = data.toString().split(" ")[1];
     const responseStatus = path === "/" ? "200 OK" : "404 NOT Found";
     socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
