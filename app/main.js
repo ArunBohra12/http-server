3ac31ba4a72219e717964d74db488958220d4626
Arun Bohra <arunbohra12@gmail.com>
2025-02-07 20:54:36 +0530

    add 404

--- app/main.js
+++ app/main.js
@@ -1,8 +1,17 @@
 const net = require("net");
 
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
-  socket.on("close", () => socket.end());
+  socket.on("data", (data) => {
+    // console.log(data.toString().split("\r\n")[0]);
+    const path = data.toString().split(" ")[1];
+    const responseStatus = path === "/" ? "200 OK" : "404 NOT Found";
+    socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
+  });
+
+  socket.on("close", () => {
+    socket.end();
+    server.close();
+  });
 });
 
 server.listen(4221, "localhost");
