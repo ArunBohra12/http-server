00f852b23c2bf1070d7d55e0e6ca1a0a4ec628a2
Arun Bohra <arunbohra12@gmail.com>
2025-02-09 16:07:23 +0530

    Update for header

--- app/main.js
+++ app/main.js
@@ -9,40 +9,59 @@ const formatHeaders = (headers) => {
   return formattedHeader;
 };
 
-const registeredPaths = ["/", "/echo/:text"];
+const setHeader = (headerKey, headerValue, headerObj) => {
+  headerObj[headerKey] = headerValue;
+};
+
+const registeredPaths = ["/", "/echo/:text", "/user-agent"];
 const matchRegisteredPath = (path) => {
   return registeredPaths.find((currentPath) => currentPath === path);
 };
 
+const getHeader = (headerToGet, headers) => {
+  return headers.find(
+    (header) => header.split(":")[0].toLowerCase() === headerToGet.toLowerCase()
+  );
+};
+
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    const path = data.toString().split(" ")[1];
+    const reqData = data.toString().split("\r\n");
+    const path = reqData[0].split(" ")[1];
+    const reqHeaders = reqData.slice(1, reqData.length - 2);
     let responseBody = "";
-
-    const headers = {
-      "Content-Type": "text/plain",
-      "Content-Length": 3,
-    };
 
     const isPathRegistered = matchRegisteredPath(path);
     const statusCode = isPathRegistered ? HTTP.HTTP_OK : HTTP.HTTP_NOT_FOUND;
     const responseStatus = HTTP.getResponseStatus(statusCode);
+
+    const headers = {
+      "Content-Type": "text/plain",
+      "Content-Length": responseBody.length,
+    };
 
     if (path.indexOf("/echo/") === 0) {
       const text = path.split("/")[2];
       responseBody += text;
     }
 
+    if (path === "/user-agent") {
+      const userAgentHeader = getHeader("User-Agent", reqHeaders);
+      responseBody += userAgentHeader;
+
+      setHeader("User-Agent", userAgentHeader, headers);
+      setHeader("Content-Length", responseBody.length, headers);
+      setHeader("Content-Length", responseBody.length, headers);
+    }
+
     const formattedHeaders = formatHeaders(headers);
     socket.write(
-      `HTTP/1.1 ${responseStatus}\r\n${formattedHeaders}\r\n\r\n${responseBody}`
+      `HTTP/1.1 ${responseStatus}\r\n${formattedHeaders}\r\n${responseBody}`
     );
   });
 
-  socket.on("close", () => {
-    socket.end();
-    server.close();
-  });
+  socket.on("close", () => socket.end());
+  socket.on("error", (err) => console.log(err.message));
 });
 
 server.listen(4221, "localhost");
